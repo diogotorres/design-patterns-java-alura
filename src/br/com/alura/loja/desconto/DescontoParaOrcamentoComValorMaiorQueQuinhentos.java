e848ee847832f591859d61c1203006b9a2013060
Diogo Torres <diogo.torres93@gmail.com>
2021-04-26 11:44:55 -0300

    implementando o template method

--- src/br/com/alura/loja/desconto/DescontoParaOrcamentoComValorMaiorQueQuinhentos.java
+++ src/br/com/alura/loja/desconto/DescontoParaOrcamentoComValorMaiorQueQuinhentos.java
@@ -5,15 +5,17 @@ import br.com.alura.loja.orcamento.Orcamento;
 import java.math.BigDecimal;
 
 public class DescontoParaOrcamentoComValorMaiorQueQuinhentos extends Desconto {
+
     public DescontoParaOrcamentoComValorMaiorQueQuinhentos(Desconto proximo) {
         super(proximo);
     }
 
-    public BigDecimal calcular(Orcamento orcamento) {
-        if (orcamento.getValor().compareTo(new BigDecimal("500")) > 0) {
-            return orcamento.getValor().multiply(new BigDecimal("0.05"));
-        }
+    public BigDecimal efetuarCalculo(Orcamento orcamento) {
+        return orcamento.getValor().multiply(new BigDecimal("0.05"));
+    }
 
-        return proximo.calcular(orcamento);
+    @Override
+    public boolean deveAplicar(Orcamento orcamento) {
+        return orcamento.getValor().compareTo(new BigDecimal("500")) > 0;
     }
 }
